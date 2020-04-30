053d24e30513cc0f3f81272138ed088066c6c7b4
Andrew Bjork <bjorkandrew1@gmail.com>
2020-04-30 13:51:32 -0500

    update

--- public/app.js
+++ public/app.js
@@ -137,11 +137,12 @@ $(document).on("click", "#make-new", function () {
       total: $("#total").val(),
       created: Date.now(),
     },
+    
   })
     // If that API call succeeds, add the title and a delete button for the note to the page
     .then(function (data) {
       // Add the title and delete button to the #results section
-
+      location.reload(true);
       $("#results").prepend(
         `<li class = "data-entry" data-id ="` +
           data._id +
